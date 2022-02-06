f9e58b5274048f168da2f39b36ca9cf1340f315f
Stephanie Duong <sduong@macalester.edu>
2022-02-05 20:55:02 -0600

    good bye

--- fullstack-course4/module04_sol/easier/SpeakGoodBye.js
+++ fullstack-course4/module04_sol/easier/SpeakGoodBye.js
@@ -11,7 +11,7 @@
     var byeSpeaker = {};
 
     // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-    var speakWord = "Goodbye";
+    var speakWord = "Good Bye";
 
     // STEP 8: Rewrite the 'speak' function such that it is attached to the
     // byeSpeaker object instead of being a standalone function.
